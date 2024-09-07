ec658ca80dec8b3394c85819f087297e53e2631e
AlexHan1237 <alex.han12307@outlook.com>
2024-09-06 22:40:33 -0400

    minor fixes

--- src/pages/SoftwareOverviewPage.js
+++ src/pages/SoftwareOverviewPage.js
@@ -7,15 +7,15 @@ function SoftwareOverviewPage() {
             In robotics, software plays a crucial role in designing, controlling, and programming robots. Here are
             some key types of software used in the field:<p />
             <ul>
-                <li>Robot Controller Programming: Various actuators in a FTC robot are contorlled by an on-board computer system called
+                <li><strong>Robot Controller Programming:</strong> Various actuators in a FTC robot are contorlled by an on-board computer system called
                     REV ControlHub. The ControlHub runs an Android OS. The control logic is progrmmed in Java using Android Studio IDE.
                     Special FTC qualified hardware components, such as DC motors, servo motors, and sensors, are interfaced with REV
                     ControlHub by a FTC SDK. Short programs called TeleOps are developed to drive the hardware components by calling the
-                    SDK functions. We will show detailed steps to install and set the TeleOps development environment.
+                    SDK functions. We will show detailed steps to install and set the TeleOps development environment.<p/>
                 </li>
-                <li>Robot Design Using CAD: CAD software allows for highly precise and accurate designs of robotic components and systems.
+                <li><strong>Robot Design Using CAD:</strong> CAD software allows for highly precise and accurate designs of robotic components and systems.
                     In FTC robot design, since CAD models of many pre-built components are linked with OnShape web application, we will
-                    share a tutorial of basic OnShape modeling in this chapter.
+                    share a tutorial of basic OnShape modeling techniques in this chapter.
                 </li>
             </ul>
         </Container>)
