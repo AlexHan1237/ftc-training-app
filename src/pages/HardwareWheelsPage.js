f7519fd741dcf05db8fc44bc89de25e3e2c6238d
AlexHan1237 <alex.han12307@outlook.com>
2024-09-09 22:50:26 -0400

    edited the wheel page

--- src/pages/HardwareWheelsPage.js
+++ src/pages/HardwareWheelsPage.js
@@ -5,10 +5,10 @@ function HardwareWheelsPage() {
         <Container>
             <Row className="px-4 my-5">
                 <Col xs={2} sm={2}>
-                    <Image src={require("./img/spur_gears.gif")} fluid />
+                    <Image src={require("./img/Traction-Wheel.webp")} fluid />
                 </Col>
                 <Col sm={8}>
-                    <h2> Standard Wheels </h2>
+                    <h2> Traction Wheels </h2>
                     These wheels are a much smaller version of the wheels on cars and also operate the same way. These wheels are good
                     at moving in one direction, particularly moving forwards. However, these wheels are unable to turn without rotating
                     the wheels themselves causing them to not be as used as the other two wheels listed. <p />
@@ -18,7 +18,7 @@ function HardwareWheelsPage() {
 
             <Row className="px-4 my-5">
                 <Col xs={2} sm={2}>
-                    <Image src={require("./img/gear_terms.jpg")} fluid />
+                    <Image src={require("./img/Omni_Wheel.webp")} fluid />
                 </Col>
                 <Col sm={8}>
                     <h2> Omni Wheels </h2>
@@ -26,22 +26,38 @@ function HardwareWheelsPage() {
                     able to move left and right. This allows for greater precision and speed as the robot well be able to move left
                     or right without having to rotate the robot wheels itself. These wheels however lack the ability to move diagonally
                     in any direction which is why most teams use mecanum wheels. <p />
+                    Omni Wheels have rollers mounted around the outside which passively rotate at 90 degrees to the powered 
+                    direction of the wheel. Omni Wheels can be used to make it easier to turn with skid steer robots or can 
+                    be used to make more advanced robot drive trains like kiwi drive. Omni Wheels can be double stacked to 
+                    create a continuous outside roller surface. <p/>
                 </Col>
             </Row >
 
 
             <Row className="px-4 my-5">
                 <Col xs={2} sm={2}>
-                    <Image src={require("./img/spur_gears.png")} fluid />
-                    <Image src={require("./img/rack_pinion.webp")} fluid />
-                    <Image src={require("./img/bevel_gears.jpg")} fluid />
+                    <Image src={require("./img/Mecanum_Wheel.png")} fluid />
+                    <Image src={require("./img/mecanum_diagram.webp")} fluid />
                 </Col>
                 <Col sm={8}>
                     <h2> Mecanum Wheels </h2>
-                    These wheels are what most teams use for their robots since these wheels are able to move in all directions.
-                    This allows teams to be very precise with their movements and also very efficient with their movements. 
-                    These wheels are the most expensive of the wheels listed but grant the most rewards and are overall the best
-                    wheels for robotics. <p />
+                    Mecanum wheels are often used in robotics competitions like FIRST Tech Challenge because they allow robots 
+                    to move in all directions, which can be helpful for navigating the competition's terrain. <p />
+
+                    Mecanum wheels are made up of a central hub and a set of rollers mounted at an angle around the perimeter 
+                    of the wheel. Each roller is typically positioned at a 45-degree angle relative to the wheel’s axis. <p/>
+
+                    The rollers are arranged in a way that each roller’s axis is angled such that they form a cross pattern 
+                    when viewed from above. This angle is crucial for the wheels' omni-directional capabilities. By varying 
+                    the speeds and directions of each wheel independently, the robot can achieve complex movements such as 
+                    diagonal motion, precise positioning, and smooth rotations.<p/>
+
+                    The control algorithms for mecanum-wheeled robots can be complex, as they require precise coordination 
+                    of wheel speeds to achieve desired movements.<p/>
+
+                    Compared to the Omni wheel, the mecanum wheel performs better over bumps than the Omni wheel because 
+                    it allows for a car-like wheel arrangement. On the other hand, many products of Omni wheels have slimmer 
+                    widths per wheel diameters than mecanum wheels.<p/>
                 </Col>
             </Row >
 
